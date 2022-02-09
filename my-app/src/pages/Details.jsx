f55a268c11a101eaafe25c0f2c7bfe3685600352
Egor <goldenyakegor@gmail.com>
2022-02-09 23:01:25 +0300

    add some functions

--- my-app/src/pages/Details.jsx
+++ my-app/src/pages/Details.jsx
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {useNavigate, useParams} from "react-router-dom";
+import {IoArrowBackCircleOutline} from "react-icons/io5";
+import styled from "styled-components";
+import axios from "axios";
+import {searchByCountry} from "../config";
+
+const Button = styled.button`
+  border: none;
+  background: none;
+  font-size: var(--fs-md);
+  display: flex;
+  gap: 5px;
+  align-items: center;
+  margin-bottom: 50px;
+  cursor: pointer;
+  
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 
 const Details = () => {
 
-    const params = useParams();
+    const [country, setCountry] = useState(null);
+
+    console.log(country)
+
+    const {name} = useParams();
     const navigate = useNavigate();
 
+    useEffect(() => {
+        axios.get(searchByCountry(name)).then(({data}) => {
+            setCountry(data[0])
+        })
+    }, [name])
+
     return (
-        <div onClick={() => navigate(-1)}>
-            Details {params.name}
+        <div>
+            <Button onClick={() => navigate(-1)}>
+                <IoArrowBackCircleOutline style={{fontSize: "22px"}}/>
+                Назад
+            </Button>
+
+            Details {name}
         </div>
     );
 };
